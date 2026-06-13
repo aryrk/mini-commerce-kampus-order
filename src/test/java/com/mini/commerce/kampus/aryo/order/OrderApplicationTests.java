20d25c42cd2695d51c75aa83be878f96954a3716
aryo.rakatama.e <aryo.rakatama.hls04@gmail.com>
2026-06-13 14:07:54 +0700

    Init

---
+++ src/test/java/com/mini/commerce/kampus/aryo/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.commerce.kampus.aryo.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
